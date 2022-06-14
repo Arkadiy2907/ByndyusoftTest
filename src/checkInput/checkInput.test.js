96d86dbaf6c5fe8c0c095dc41441dffd94f51da7
Arkadiy2907 <arkadiy2907@gmail.com>
2022-06-14 21:36:34 +0300

    fix6 unit test on english

--- src/checkInput/checkInput.test.js
+++ src/checkInput/checkInput.test.js
@@ -1,16 +1,16 @@
 const checkInput = require('./checkInput');
 
 describe('checkInput', () => {
-	test('валидация при пустой строке', () => {
-		expect(checkInput(' ')).toBe(undefined);
+	test('validation for empty string', () => {
+		expect(checkInput(' ')).toBeUndefined();
 	})
-	test('есть строка', () => {
+	test('there is a string', () => {
 		expect(checkInput('1, 2, 3')).toEqual([1, 2, 3]);
 	})
-	test('есть смешенная строка', () => {
+	test('there is a mixed string', () => {
 		expect(checkInput('1a, a2, 3')).toEqual([1, 3]);
 	})
-	test('есть неволидная строка строка', () => {
+	test('there is not valid a string', () => {
 		expect(checkInput('s1, a, a3d')).toEqual([]);
 	})
 })
